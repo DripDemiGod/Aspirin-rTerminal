f09206c9eccec187fe8e91fc9ac0f1871664fe33
DripDemiGod <toxicflamez013@gmail.com>
2022-05-12 17:38:15 +1200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
+if not syn then kick("\nUnsupported Exploit.\nrTerminal is for Synapse - X\nONLY") end
 local start = tick()
+local running = true
 local client = game:GetService('Players').LocalPlayer;
 local usedCache = shared.__urlcache and next(shared.__urlcache) ~= nil
+rconsolename("rTerminal Loading...")
 
 --http stuff
 shared.__urlcache = shared.__urlcache or {}
@@ -34,13 +37,12 @@ end
 local metadata = urlLoad("https://raw.githubusercontent.com/DripDemiGod/Asprin-rTerminal/main/metadata.lua")
 local httpService = game:GetService('HttpService')
 
-
 --Command Functions
 local function help(ctable)
 	print("Here is a list of available commands: ")
 	for i, v in pairs(ctable) do
 		--print(tostring(ctable[i][1])) --PRINTS FUNCTION ID
-		print(ctable[i][2])
+		rconsoleprint(ctable[i][2].. "\n")
 	end
 end
 
@@ -50,11 +52,21 @@ local function add(ctable, var1, var2)
 	end
 end
 
+local function clear(ctable)
+    rconsoleclear()
+end
+
+local function quit(ctabke)
+    running = false
+end
+
 --Main Command Dictionary
 --Adding commands is simple. ["commandIndex"] = {commandFunction, "descriptionString"}
 local Commands = {
 	["help"] = {help, "'help': Gives brief information about the terminal and returns all commands available."},
-	["add"] = {add, "'add': Requires 2 varaibles. Adds them together and prints the output."}
+	["add"] = {add, "'add': Requires 2 varaibles. Adds them together and prints the output."},
+    ["clear"] = {clear, "'clear': Clears the rTerminal console."},
+    ["quit"] = {quit, "'quit': Closes the terminal and terminates the script."}
 }
 
 --[[
@@ -71,4 +83,29 @@ else
 	print("Command not found. Please use the command 'help' for a list of available commands.")
 end
 --]]
-rconsoleprint(string.format('Loaded script in %.4f second(s)!', tick() - start), 3)
+local function inputFunc()
+    rconsoleprint("\nC:\rTerminal>")
+    local commandInput = rconsoleinput()
+    if commandInput then
+        if Commands[commandInput] then
+            Commands[commandInput][1](Commands)
+        else
+            rconsoleprint("Unknown command. Please use the command 'help' for a list of available commands. \n")
+        end
+    end
+end
+rconsolename("rTerminal ver: ".. metadata.version)
+rconsoleprint(string.format('Loaded script in %.4f second(s)!', tick() - start, 3))
+rconsoleprint("\nVersion: ".. metadata.version)
+rconsoleprint("\nLastest Updated: ".. metadata.updated)
+rconsoleprint("\n".. metadata.message)
+rconsoleprint("Use the 'clear' command to clear this message.")
+rconsoleprint("You can turn these off by default in the 'rterminal.config' file.")
+rconsoleprint("------------------------------------------------------------------")
+
+while running == true do
+    task.wait()
+    inputFunc()
+end
+rconsoleclear()
+rconsolename("rTerminal task terminated.")
