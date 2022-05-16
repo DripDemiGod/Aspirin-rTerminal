04ff04a89adda2cd09726ca977bfee5fc4f5725f
Drip <toxicflamez013@gmail.com>
2022-05-16 19:24:46 +1200

    Update metadata.lua

--- metadata.lua
+++ metadata.lua
@@ -1,6 +1,6 @@
 return {
-	version = "beta1.0",
+	version = "beta1.1",
 	updated = "12/05/2022 11:23 AM",
-	message = "This script is in BETA! Not everything may work."
+	message = "This script is in BETA, not everything may work."
 }
 -- Drip God was here
