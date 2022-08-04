ab6ca6e97251d30930227cbf374971796416b543
DripDemiGod <toxicflamez013@gmail.com>
2022-08-04 18:15:34 +1200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 --https://api.github.com/repos/DripDemiGod/Aspirin-rTerminal/contents/commands?ref=main
-
+httpService = game:GetService("HttpService")
 local rawData = httpService:GetAsync("https://api.github.com/repos/DripDemiGod/Aspirin-rTerminal/contents/commands?ref=main")
 local jsonData = httpService:JSONDecode(rawData)
 print(jsonData)
